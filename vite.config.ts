fcb5f571e814e71db8aafae8147355934cf89a1b
yhaeberl <yhaeberl@students.uni-mainz.de>
2025-05-11 22:33:10 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '/Bachelorarbeit/',
   assetsInclude: ['**/*.glsl'],
+  build: {
+    outDir: 'docs'  // ← Vite wird direkt hier rein bauen!
+  }
 });
