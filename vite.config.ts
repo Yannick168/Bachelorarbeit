fc61093c1b9ab59ea02d550c54c7e22e76d93c1c
yhaeberl <yhaeberl@students.uni-mainz.de>
2025-05-11 22:41:54 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/Bachelorarbeit/',
   assetsInclude: ['**/*.glsl'],
   build: {
-    outDir: 'docs'  // ← Vite wird direkt hier rein bauen!
-  }
+    outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        surface: resolve(__dirname, 'parametricSurface_01.html'),
+      },
+    },
+  },
 });
