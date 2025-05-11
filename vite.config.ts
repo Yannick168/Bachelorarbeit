03aa8dda85d1d45927885df9535ff6005a9e5cdd
yhaeberl <yhaeberl@students.uni-mainz.de>
2025-05-12 00:04:53 +0200

    three.js test

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       input: {
         triangle: resolve(__dirname, 'index.html'),
         surface_01: resolve(__dirname, 'parametricSurface_01.html'),
+        surface_02: resolve(__dirname, 'parametricSurface_02.html')
       }
     }
   }
