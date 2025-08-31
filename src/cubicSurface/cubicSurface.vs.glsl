d1bc579dd54d7ab6220492712937c0561f3e8784
yhaeberl <yhaeberl@students.uni-mainz.de>
2025-08-31 20:55:52 +0200

    culling bug

--- src/cubicSurface/cubicSurface.vs.glsl
+++ src/cubicSurface/cubicSurface.vs.glsl
@@ -1,8 +1,13 @@
-// VS
-in vec3 aPosition;
-uniform mat4 uProjection, uModelView; // = View * Model
-out vec3 vUV;                         // Modellraum-Position
+#version 300 es
+precision highp float;
+
+in vec3 aPosition;     // kommt aus deinem Cube-VBO
+out vec3 vUV;          // geht weiter an den Fragment-Shader
+
+uniform mat4 uProjection;
+uniform mat4 uModelView;
+
 void main() {
-  vUV = aPosition;                    // Modellraum!
+  vUV = aPosition;                           // im Modelraum
   gl_Position = uProjection * uModelView * vec4(aPosition, 1.0);
 }
