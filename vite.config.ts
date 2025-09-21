0d9c22f6ddea08b351879f9889e689ae35b57f28
yhaeberl <yhaeberl@students.uni-mainz.de>
2025-09-21 14:48:24 +0200

    Widget iframes

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,15 @@ export default defineConfig({
     rollupOptions: {
       input: {
         catenoid: resolve(__dirname, 'src/catenoid/catenoid.html'),
+        catenoidWidget: resolve(__dirname, 'src/catenoid/catenoidWidget.html'),
         catenoidHelicoid: resolve(__dirname, 'src/catenoidHelicoid/catenoidHelicoid.html'),
+        catenoidHelicoidWidget: resolve(__dirname, 'src/catenoidHelicoid/catenoidHelicoidWidget.html'),
         trochoid: resolve(__dirname, 'src/trochoid/trochoid.html'),
+        trochoidWidget: resolve(__dirname, 'src/trochoid/trochoidWidget.html'),
         epitrochoid: resolve(__dirname, 'src/epitrochoid/epitrochoid.html'),
+        epitrochoidWidget: resolve(__dirname, 'src/epitrochoid/epitrochoidWidget.html'),
         hypotrochoid: resolve(__dirname, 'src/hypotrochoid/hypotrochoid.html'),
+        hypotrochoidWidget: resolve(__dirname, 'src/hypotrochoid/hypotrochoidWidget.html'),
         surfaceEvolver: resolve(__dirname, 'src/surfaceEvolver/surfaceEvolver.html'),
         cubicSurface: resolve(__dirname, 'src/cubicSurface/cubicSurface.html'),
         cubicSurfaceWidget: resolve(__dirname, 'src/cubicSurface/cubicSurfaceWidget.html'),
